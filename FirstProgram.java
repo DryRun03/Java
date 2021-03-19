9bf3fa4d44c83bdcf4ae1e0cdb2bfe6a678d3832
DryRun03 <56753238+DryRun03@users.noreply.github.com>
2021-03-19 16:20:53 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+public class FirstProgram 
+{
+        public static void main(String [] args)
+        {
+            System.out.println("Prnting is fun!");
+            System.out.println("Java> C++");
+            System.out.println("I spilled Java all Over my paper,");
+            System.out.println("My dog ate my Java");
+        }
+}
+
---
+++ Initials.java
@@ -0,0 +1,12 @@
+public class Initials {
+    public static void main(String[] args)
+    {
+    System.out.println("PPPPP    GGGGG     ");
+    System.out.println("P   P    G   ");
+    System.out.println("PPPPP    G   ");
+    System.out.println("P        G GGG");
+    System.out.println("p        G   G");
+    System.out.println("P        GGGGG  ");
+    }
+}
+
