597055814ed7e8338a1e341c16e800c47fa4e93e
DryRun03 <56753238+DryRun03@users.noreply.github.com>
2021-03-31 11:01:03 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse
+{
+    public static void main(String[] args)
+    {
+           int grade = 45;
+           if(grade >= 50)
+           {
+               System.out.println("Great Job, You Passed!");
+           }
+            else
+           {
+               System.out.println("You Failed!");
+           }
+    }
+}
